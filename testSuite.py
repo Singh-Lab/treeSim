50210d0479e782bbf157d304f60894964b640fce
Chaitanya Aluru <chaitanya@nat-oitwireless-inside-vapornet100-10-8-50-46.princeton.edu>
2019-02-19 01:35:55 -0500

    Propagating Changes

--- testSuite.py
+++ testSuite.py
@@ -1,7 +1,8 @@
 #Test suite for HostTreeGen and GuestTreeGen
-from HostTreeGen import genHostTree, gaussNoise
-from GuestTreeGen import buildGuestTree, s3, expfunc
+from HostTreeGen import genHostTree
+from GuestTreeGen import buildGuestTree
 from Utils import printProgressBar
+from TreeUtils import s3, expfunc, gaussNoise
 import numpy as np
 
 #Basic test to ensure errors aren't encountered
