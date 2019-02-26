b84224a61f762e8c724955f36e24c205fe9f6865
Chaitanya <chaitanya.aluru@gmail.com>
2019-02-26 17:21:03 -0500

    Started writing branch relaxation methods

--- BranchRelaxer.py
+++ BranchRelaxer.py
@@ -1,1 +1,20 @@
 #Implements various models of branch relaxation
+from ete3 import Tree
+import stats
+
+def uncorrelatedRelaxation(tree, method='exponential'):
+    """
+    Relaxes each branch length individually without an explicit time process.
+    Multiplies each branch length by a rate parameter drawn from the specified
+    distribution. Available distributions are 'exponential', 'gamma', or 'gaussian'
+
+    Args:
+        tree   (Tree): The ete3 Tree with branch lengths to relax
+        method (str ): The distribution to draw rates from. Options are 'exponential',
+                       'gamma', and 'gaussian'. Default is exponential, others will
+                       default to exponential
+    """
+
+    rateFunc = stats.exp(1)
+    if method == 'gamma': rateFunc = stats.gamma
+    if method == 'gaussian' : rateFunc = stats.gaussian()
--- stats.py
+++ stats.py
@@ -1,14 +1,20 @@
 #Holds useful statistical models for generating trees
 import numpy as np
-from scipy.stats import expon
+from scipy import stats
 
 def expfunc(minimum=1, maximum=3):
     """Exponential distribution with lambda=0.75 and min/max parameters"""
-    return min(maximum, max(minimum, int(expon(0.75).rvs())))
+    return min(maximum, max(minimum, int(stats.expon(0.75).rvs())))
 
 def gaussNoise(n):
     """"Returns a float drawn from N(n, .1*n)"""
     return np.random.normal(n, .1*n)
 
-def exp(x):
-    return expon(-1 * x)
+def exp(l):
+    return lambda x: l * stats.expon(-1 * l * x).rvs()
+
+def gamma(x):
+    return stats.gamma(x, x**2).rvs()
+
+def gaussian():
+    return lambda x: np.random.normal(x, x)
